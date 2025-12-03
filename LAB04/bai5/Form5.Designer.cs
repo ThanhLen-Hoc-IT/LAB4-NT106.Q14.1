a827b8741192e90814ed83e30c0ea7da14b4f583
Tri Huynh <trihyunhminh@gmail.com>
2025-12-03 22:37:13 +0700

    sua loi dang ky

--- LAB04/bai5/Form5.Designer.cs
+++ LAB04/bai5/Form5.Designer.cs
@@ -22,110 +22,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblTitle = new System.Windows.Forms.Label();
-            this.lblUsername = new System.Windows.Forms.Label();
-            this.lblPassword = new System.Windows.Forms.Label();
-            this.txtUsername = new System.Windows.Forms.TextBox();
-            this.txtPassword = new System.Windows.Forms.TextBox();
-            this.btnPOST = new System.Windows.Forms.Button();
-            this.lblResult = new System.Windows.Forms.Label();
-            this.rtbResult = new System.Windows.Forms.RichTextBox();
-            this.SuspendLayout();
+            lblTitle = new Label();
+            lblUsername = new Label();
+            lblPassword = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            btnPOST = new Button();
+            lblResult = new Label();
+            rtbResult = new RichTextBox();
+            SuspendLayout();
             // 
             // lblTitle
             // 
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(30, 20);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(437, 30);
-            this.lblTitle.TabIndex = 0;
-            this.lblTitle.Text = "HTTP POST: ĐĂNG NHẬP VỚI API (BÀI 5)";
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitle.Location = new Point(65, 49);
+            lblTitle.Margin = new Padding(6, 0, 6, 0);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(864, 59);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "HTTP POST: ĐĂNG NHẬP VỚI API (BÀI 5)";
             // 
             // lblUsername
             // 
-            this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(50, 70);
-            this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(65, 13);
-            this.lblUsername.TabIndex = 1;
-            this.lblUsername.Text = "Username:";
+            lblUsername.AutoSize = true;
+            lblUsername.Location = new Point(108, 172);
+            lblUsername.Margin = new Padding(6, 0, 6, 0);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(126, 32);
+            lblUsername.TabIndex = 1;
+            lblUsername.Text = "Username:";
             // 
             // lblPassword
             // 
-            this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(50, 105);
-            this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(60, 13);
-            this.lblPassword.TabIndex = 2;
-            this.lblPassword.Text = "Password:";
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(108, 258);
+            lblPassword.Margin = new Padding(6, 0, 6, 0);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(116, 32);
+            lblPassword.TabIndex = 2;
+            lblPassword.Text = "Password:";
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(120, 70);
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(250, 20);
-            this.txtUsername.TabIndex = 3;
-            this.txtUsername.Text = "phatpt"; // Giá trị mặc định
+            txtUsername.Location = new Point(260, 172);
+            txtUsername.Margin = new Padding(6, 7, 6, 7);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(537, 39);
+            txtUsername.TabIndex = 3;
+            txtUsername.Text = "phatpt";
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(120, 105);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.PasswordChar = '•'; 
-            this.txtPassword.Size = new System.Drawing.Size(250, 20);
-            this.txtPassword.TabIndex = 4;
-            this.txtPassword.Text = "123456"; // Giá trị mặc định
+            txtPassword.Location = new Point(260, 258);
+            txtPassword.Margin = new Padding(6, 7, 6, 7);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '•';
+            txtPassword.Size = new Size(537, 39);
+            txtPassword.TabIndex = 4;
+            txtPassword.Text = "123456";
             // 
             // btnPOST
             // 
-            this.btnPOST.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPOST.Location = new System.Drawing.Point(376, 70);
-            this.btnPOST.Name = "btnPOST";
-            this.btnPOST.Size = new System.Drawing.Size(120, 55);
-            this.btnPOST.TabIndex = 5;
-            this.btnPOST.Text = "ĐĂNG NHẬP (POST)";
-            this.btnPOST.UseVisualStyleBackColor = true;
+            btnPOST.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnPOST.Location = new Point(815, 172);
+            btnPOST.Margin = new Padding(6, 7, 6, 7);
+            btnPOST.Name = "btnPOST";
+            btnPOST.Size = new Size(260, 135);
+            btnPOST.TabIndex = 5;
+            btnPOST.Text = "ĐĂNG NHẬP (POST)";
+            btnPOST.UseVisualStyleBackColor = true;
             // 
             // lblResult
             // 
-            this.lblResult.AutoSize = true;
-            this.lblResult.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblResult.Location = new System.Drawing.Point(50, 140);
-            this.lblResult.Name = "lblResult";
-            this.lblResult.Size = new System.Drawing.Size(130, 17);
-            this.lblResult.TabIndex = 6;
-            this.lblResult.Text = "Kết quả Phản hồi:";
+            lblResult.AutoSize = true;
+            lblResult.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblResult.Location = new Point(108, 345);
+            lblResult.Margin = new Padding(6, 0, 6, 0);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(231, 36);
+            lblResult.TabIndex = 6;
+            lblResult.Text = "Kết quả Phản hồi:";
             // 
             // rtbResult
             // 
-            this.rtbResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.rtbResult.Location = new System.Drawing.Point(53, 165);
-            this.rtbResult.Name = "rtbResult";
-            this.rtbResult.ReadOnly = true;
-            this.rtbResult.Size = new System.Drawing.Size(443, 200);
-            this.rtbResult.TabIndex = 7;
-            this.rtbResult.Text = "Thông tin token sẽ hiển thị tại đây sau khi đăng nhập thành công...";
+            rtbResult.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            rtbResult.Location = new Point(115, 406);
+            rtbResult.Margin = new Padding(6, 7, 6, 7);
+            rtbResult.Name = "rtbResult";
+            rtbResult.ReadOnly = true;
+            rtbResult.Size = new Size(955, 486);
+            rtbResult.TabIndex = 7;
+            rtbResult.Text = "Thông tin token sẽ hiển thị tại đây sau khi đăng nhập thành công...";
             // 
             // Form5
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(550, 400); 
-            this.Controls.Add(this.rtbResult);
-            this.Controls.Add(this.lblResult);
-            this.Controls.Add(this.btnPOST);
-            this.Controls.Add(this.txtPassword);
-            this.Controls.Add(this.txtUsername);
-            this.Controls.Add(this.lblPassword);
-            this.Controls.Add(this.lblUsername);
-            this.Controls.Add(this.lblTitle);
-            this.Name = "Form5";
-            this.Text = "Bài 5: HTTP POST - Đăng nhập";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1192, 985);
+            Controls.Add(rtbResult);
+            Controls.Add(lblResult);
+            Controls.Add(btnPOST);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(lblPassword);
+            Controls.Add(lblUsername);
+            Controls.Add(lblTitle);
+            Margin = new Padding(6, 7, 6, 7);
+            Name = "Form5";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Bài 5: HTTP POST - Đăng nhập";
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
