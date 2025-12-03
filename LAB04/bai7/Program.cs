baa242900ed7907a95a36e35b53b077c7e3fbe1d
trihuynhminh <trihuynhminh2006@gmail.com>
2025-12-03 15:41:56 +0700

    tr oi met qua (#5)
    * tr oi met qua
    
    * dau cai l qua
    
    ---------
    
    Co-authored-by: Tri Huynh <trihyunhminh@gmail.com>

---
+++ LAB04/bai7/Program.cs
@@ -0,0 +1,20 @@
+using Lab4.Bai7;
+using Lab4.Bai7.Final;
+
+namespace bai7
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Final());
+        }
+    }
+}
